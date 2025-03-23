7f4f329574b003bb4a13e7bdd7b0f044d24d951a
vadim_1975 <75vadimalena96@gmail.com>
2025-03-23 14:16:15 +0200

    add task

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const nameInput = document.querySelector("#name-input");
-const nameOutput = document.querySelector("#name-output");
-
-nameInput.addEventListener("input", (event) => {
-  const trimmedValue = event.target.value.trim();
-  nameOutput.textContent = trimmedValue || "Anonymous";
-});
